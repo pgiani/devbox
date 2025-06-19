aa3dadd7b8c555e66dd2fb1c77f6494053cdf1cb
Giani, Pablo H <pablo.giani@charter.com>
2025-06-19 09:23:01 -0600

    removed lock

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ export default function App() {
   return (
     <div className="App">
       {/* TASK 1 */}
-      {/* <h1> Joe Pizza </h1>*/}
+       <h1> Joe Pizza </h1> 
       {/*<h2>Total price of pizza in Euros:</h2> */}
       {/* EUR: {pizzaPrice()} */}
       {/* TASK 2 */}
